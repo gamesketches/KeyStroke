751ba97bd54880f69d7816dc56b7ff3f61ac8eea
gamesketches <sam.von.ehren@gmail.com>
2017-04-20 22:44:21 -0400

    Got keys switching, put in scrollview for chatlog

---
+++ Assets/Scripts/InputChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputChecker : MonoBehaviour {
+
+	public class Sentence {
+		public string call;
+		public string[] wordsToCheckFor;
+		public string[] missedResponses;
+	}
+
+	Queue<Sentence> chatLog;
+	Text chatText;
+	// Use this for initialization
+	void Start () {
+		chatText = GetComponentInChildren<Text>();
+		chatText.text = "Hey";
+		chatText.text += "\nYo";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
